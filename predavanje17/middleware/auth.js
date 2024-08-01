7e3b96424442b3eca04a3fdee9fcdcbc406eca21
milanka <mina.m81@gmail.com>
2024-08-01 19:39:17 +0200

    auth fixed

--- predavanje17/middleware/auth.js
+++ predavanje17/middleware/auth.js
@@ -1,18 +1,16 @@
-const JWTData = require("../config/JWT_SECRET");
+const { JWT_SECRET } = require("../config/JWT_SECRET");
 const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
-  const authData = req.header("Authorization");
-  if (authData === undefined) {
-    res.status(400).send("Token is not defined");
+  if (!req.headers.authorization) {
+    return res.status(400).send("Token is not defined");
   }
-
-  const token = req.header("Authorization").replace("Bearer ", "");
-
   try {
-    const decoded = jwt.verify(token, JWTData.JWT_SECRET);
+    const token = req.header("authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, JWT_SECRET);
     req.userId = decoded.userId;
     req.userRole = decoded.role;
+
     next();
   } catch (err) {
     res.status(401).send("Unauthorized");
