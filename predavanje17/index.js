0cf310adf8952282a4b41747cde53ee8f334e6ba
milanka <mina.m81@gmail.com>
2024-08-01 19:29:55 +0200

    removing localhost

--- predavanje17/index.js
+++ predavanje17/index.js
@@ -5,12 +5,12 @@ const blogRoutes = require("./routes/blog");
 const authService = require("./middleware/auth");
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 app.use(bodyParser.json());
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/blogs", authService.authenticate, blogRoutes);
 
-app.listen(PORT, "localhost", () => {
+app.listen(PORT, () => {
   console.log("server connected");
 });
